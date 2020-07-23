1074b76c3cb390eb820cf164366d3c76376d49b4
jsakamoto <junichi.jp@gmail.com>
2020-07-24 08:58:32 +0900

    Add sample code that downloading file via HttpClient.

--- Client/helper.ts
+++ Client/helper.ts
@@ -6,3 +6,18 @@
     anchorElement.click();
     anchorElement.remove();
 }
+
+function downloadFromByteArray(options: { byteArray: string, fileName: string, contentType: string }): void {
+    // console.log(options);
+
+    // The byte array in .NET is encoded to base64 string when it passes to JavaScript.
+    // Therefore, it has to be decoded from the base64 string and has to be converted to Uint8Array manually.
+    const uint8Array = new Uint8Array(atob(options.byteArray).split('').map(c => c.charCodeAt(0)));
+
+    const blob = new Blob([uint8Array], { type: options.contentType });
+    const url = URL.createObjectURL(blob);
+
+    downloadFromUrl({ url: url, fileName: options.fileName });
+
+    URL.revokeObjectURL(url);
+}
--- Client/wwwroot/script/helper.js
+++ Client/wwwroot/script/helper.js
@@ -8,3 +8,13 @@ function downloadFromUrl(options) {
     anchorElement.click();
     anchorElement.remove();
 }
+function downloadFromByteArray(options) {
+    // console.log(options);
+    // The byte array in .NET is encoded to base64 string when it passes to JavaScript.
+    // Therefore, it has to be decoded from the base64 string and has to be converted to Uint8Array manually.
+    var uint8Array = new Uint8Array(atob(options.byteArray).split('').map(function (c) { return c.charCodeAt(0); }));
+    var blob = new Blob([uint8Array], { type: options.contentType });
+    var url = URL.createObjectURL(blob);
+    downloadFromUrl({ url: url, fileName: options.fileName });
+    URL.revokeObjectURL(url);
+}
