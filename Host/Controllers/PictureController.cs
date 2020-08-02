e63682969c4868ec5f22969a7a4d6bd726cf7b09
jsakamoto <junichi.jp@gmail.com>
2020-08-02 15:56:05 +0900

    Add ASP.NET Core host project.

---
+++ Host/Controllers/PictureController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWasmDownload.Host.Controllers
+{
+    [ApiController]
+    public class PictureController : ControllerBase
+    {
+        private IWebHostEnvironment HostEnvironment { get; }
+
+        public PictureController(IWebHostEnvironment hostEnvironment)
+        {
+            HostEnvironment = hostEnvironment;
+        }
+
+        [HttpGet("/pictures/1")]
+        public async Task<IActionResult> GetPicture1()
+        {
+            var fileInfo = this.HostEnvironment.ContentRootFileProvider.GetFileInfo("Assets/1.jpg");
+            var pictureBytes = await System.IO.File.ReadAllBytesAsync(fileInfo.PhysicalPath);
+            return this.File(pictureBytes, "image/jpeg");
+        }
+    }
+}
