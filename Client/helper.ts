fa795722aa77af5a553855aa841881eeee256c86
jsakamoto <junichi.jp@gmail.com>
2021-10-14 22:06:56 +0900

    Upgrade to .NET 6

--- Client/helper.ts
+++ Client/helper.ts
@@ -10,9 +10,21 @@ function downloadFromUrl(options: { url: string, fileName?: string }): void {
     anchorElement.remove();
 }
 
-function downloadFromByteArray(options: { byteArray: string, fileName: string, contentType: string }): void {
-    // The byte array in .NET is encoded to base64 string when it passes to JavaScript.
-    // So we can pass that base64 encoded string to the browser as a "data URL" directly.
-    const url = "data:" + options.contentType + ";base64," + options.byteArray;
+function downloadFromByteArray(options: { byteArray: Uint8Array | string, fileName: string, contentType: string }): void {
+
+    const url = typeof (options.byteArray) === 'string' ?
+
+        // .NET 5 or earlier, the byte array in .NET is encoded to base64 string when it passes to JavaScript.
+        // In that case, that base64 encoded string can be pass to the browser as a "data URL" directly.
+        "data:" + options.contentType + ";base64," + options.byteArray :
+
+        // .NET 6 or later, the byte array in .NET is passed to JavaScript as an UInt8Array efficiently.
+        // - https://docs.microsoft.com/en-us/dotnet/core/compatibility/aspnet-core/6.0/byte-array-interop
+        // In that case, the UInt8Array can be converted to an object URL and passed to the browser.
+        // But don't forget to revoke the object URL when it is no longer needed.
+        URL.createObjectURL(new Blob([options.byteArray], { type: options.contentType }))
+
     downloadFromUrl({ url: url, fileName: options.fileName });
+
+    if (typeof (options.byteArray) !== 'string') URL.revokeObjectURL(url);
 }
--- Client/wwwroot/script/helper.js
+++ Client/wwwroot/script/helper.js
@@ -11,8 +11,16 @@ function downloadFromUrl(options) {
     anchorElement.remove();
 }
 function downloadFromByteArray(options) {
-    // The byte array in .NET is encoded to base64 string when it passes to JavaScript.
-    // So we can pass that base64 encoded string to the browser as a "data URL" directly.
-    var url = "data:" + options.contentType + ";base64," + options.byteArray;
+    var url = typeof (options.byteArray) === 'string' ?
+        // .NET 5 or earlier, the byte array in .NET is encoded to base64 string when it passes to JavaScript.
+        // In that case, that base64 encoded string can be pass to the browser as a "data URL" directly.
+        "data:" + options.contentType + ";base64," + options.byteArray :
+        // .NET 6 or later, the byte array in .NET is passed to JavaScript as an UInt8Array efficiently.
+        // - https://docs.microsoft.com/en-us/dotnet/core/compatibility/aspnet-core/6.0/byte-array-interop
+        // In that case, the UInt8Array can be converted to an object URL and passed to the browser.
+        // But don't forget to revoke the object URL when it is no longer needed.
+        URL.createObjectURL(new Blob([options.byteArray], { type: options.contentType }));
     downloadFromUrl({ url: url, fileName: options.fileName });
+    if (typeof (options.byteArray) !== 'string')
+        URL.revokeObjectURL(url);
 }
